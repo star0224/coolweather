495dccb8b91db265d0b2fddb97d789c54629eb1f
star0224 <1048950702@qq.com>
2019-04-15 16:35:31 +0800

    创建数据库和表的各项配置

---
+++ app/src/main/java/com/xin/coolweather/db/Country.java
@@ -0,0 +1,46 @@
+package com.xin.coolweather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class Country extends LitePalSupport {
+
+    private int id;
+
+    private String countryName;
+
+    private int cityId;
+
+    private int weatherId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public int getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(int weatherId) {
+        this.weatherId = weatherId;
+    }
+}
